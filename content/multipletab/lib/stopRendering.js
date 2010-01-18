4b16f43d950df80534245a0ad98f168315e6ac1d
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-01-18 10:21:29 +0000

    画面の描画を再開させたあと、表示中のポップアップがある時は強制的に再描画させるようにした
    git-svn-id: https://www.cozmixng.org/repos/piro/multipletab/trunk@6116 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/multipletab/lib/stopRendering.js
+++ content/multipletab/lib/stopRendering.js
@@ -6,13 +6,13 @@
    // do something
    window['piro.sakura.ne.jp'].stopRendering.start();
 
- lisence: The MIT License, Copyright (c) 2009 SHIMODA "Piro" Hiroshi
+ lisence: The MIT License, Copyright (c) 2009-2010 SHIMODA "Piro" Hiroshi
    http://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk/license.txt
  original:
    http://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk/stopRendering.js
 */
 (function() {
-	const currentRevision = 2;
+	const currentRevision = 3;
 
 	if (!('piro.sakura.ne.jp' in window)) window['piro.sakura.ne.jp'] = {};
 
@@ -22,6 +22,9 @@
 	if (loadedRevision && loadedRevision > currentRevision) {
 		return;
 	}
+
+	if ('destroy' in window['piro.sakura.ne.jp'].stopRendering)
+		window['piro.sakura.ne.jp'].stopRendering.destroy();
 
 	window['piro.sakura.ne.jp'].stopRendering = {
 		revision : currentRevision,
@@ -39,7 +42,7 @@
 
 		stop : function()
 		{
-			if (!this._stopLevel) {
+			if (this._stopLevel == 0) {
 				this.baswWindow.setPosition(window.top.innerWidth * 3, window.top.innerHeight * 3);
 			}
 			this._stopLevel++;
@@ -48,10 +51,78 @@
 		start : function()
 		{
 			this._stopLevel--;
-			if (!this._stopLevel) {
-				this.baswWindow.setPosition(0, 0);
+			if (this._stopLevel > 0)
+				return;
+
+			this._stopLevel = 0;
+			this.baswWindow.setPosition(0, 0);
+
+			this._popups.forEach(function(aPopup, aIndex) {
+				if (aPopup.state != 'open') return;
+				var w = aPopup.boxObject.width;
+				var h = aPopup.boxObject.height;
+				aPopup.sizeTo(w, h-1);
+				aPopup.sizeTo(w, h);
+			}, this);
+		},
+
+		onResize : function(aEvent)
+		{
+			if (aEvent.target != window || !this._stopLevel)
+				return;
+
+			this._stopLevel = 0;
+			this.start();
+		},
+
+
+		handleEvent : function(aEvent)
+		{
+			switch (aEvent.type)
+			{
+				case 'unload':
+					this.destroy();
+					return;
+
+				case 'resize':
+					this.onResize(aEvent);
+					return;
+
+				case 'popupshown':
+					let (index = this._popups.indexOf(aEvent.originalTarget)) {
+						if (index < 0)
+							this._popups.push(aEvent.originalTarget);
+					}
+					return;
+
+				case 'popuphidden':
+					let (index = this._popups.indexOf(aEvent.originalTarget)) {
+						if (index > -1)
+							this._popups.splice(index, 1);
+					}
+					return;
 			}
+		},
+
+		init : function()
+		{
+			this._popups = [];
+			window.addEventListener('resize', this, false);
+			window.addEventListener('popupshown', this, false);
+			window.addEventListener('popuphidden', this, false);
+			window.addEventListener('unload', this, false);
+		},
+
+		destroy : function()
+		{
+			this._popups = [];
+			window.removeEventListener('resize', this, false);
+			window.removeEventListener('popupshown', this, false);
+			window.removeEventListener('popuphidden', this, false);
+			window.removeEventListener('unload', this, false);
 		}
 
 	};
+
+	window['piro.sakura.ne.jp'].stopRendering.init();
 })();
