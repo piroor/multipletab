283e70d0ff0e54a0c7304f22386f599825fc290c
YUKI Hiroshi <shimoda@clear-code.com>
2017-09-11 19:43:14 +0900

    Follow to changes of TST

--- webextensions/background.js
+++ webextensions/background.js
@@ -8,21 +8,21 @@
 const kID = 'multipletab@piro.sakura.ne.jp';
 const kTST_ID = 'treestyletab@piro.sakura.ne.jp';
 
-const kCOMMAND_TST_REGISTER_LISTENER_ADDON   = `${kTST_ID}:register-listener-addon`;
-const kCOMMAND_TST_UNREGISTER_LISTENER_ADDON = `${kTST_ID}:unregister-listener-addon`;
-const kCOMMAND_TST_TAB_CLICKED               = `${kTST_ID}:notify:tab-clicked`;
-const kCOMMAND_TST_IS_SUBTREE_COLLAPSED      = `${kTST_ID}:request:is-subtree-collapsed`;
-const kCOMMAND_TST_HAS_CHILD_TABS            = `${kTST_ID}:request:has-child-tabs`;
-const kCOMMAND_TST_GET_DESCENDANT_TABS       = `${kTST_ID}:request:get-descendant-tabs`;
+const kTSTAPI_REGISTER_LISTENER_ADDON   = `${kTST_ID}:register-listener-addon`;
+const kTSTAPI_UNREGISTER_LISTENER_ADDON = `${kTST_ID}:unregister-listener-addon`;
+const kTSTAPI_NOTIFY_TAB_CLICKED        = `${kTST_ID}:notify:tab-clicked`;
+const kTSTAPI_IS_SUBTREE_COLLAPSED      = `${kTST_ID}:request:is-subtree-collapsed`;
+const kTSTAPI_HAS_CHILD_TABS            = `${kTST_ID}:request:has-child-tabs`;
+const kTSTAPI_GET_DESCENDANT_TABS       = `${kTST_ID}:request:get-descendant-tabs`;
 
 browser.runtime.sendMessage(kTST_ID, {
-  type: kCOMMAND_TST_REGISTER_LISTENER_ADDON
+  type: kTSTAPI_REGISTER_LISTENER_ADDON
 });
 
 function onMessageExternal(aMessage, aSender) {
   console.log('onMessageExternal: ', aMessage, aSender);
   switch (aMessage.type) {
-    case kCOMMAND_TST_TAB_CLICKED: {
+    case kTSTAPI_TAB_CLICKED: {
     }; break;
   }
 }
