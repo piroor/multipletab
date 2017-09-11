36d3440cda58d5923f1f4629086db8020f5b1f02
YUKI Hiroshi <shimoda@clear-code.com>
2017-09-11 20:42:55 +0900

    Toggle selected state of tabs by ctrl-click

--- webextensions/background.js
+++ webextensions/background.js
@@ -14,21 +14,44 @@ const kTSTAPI_NOTIFY_TAB_CLICKED        = `${kTST_ID}:notify:tab-clicked`;
 const kTSTAPI_IS_SUBTREE_COLLAPSED      = `${kTST_ID}:request:is-subtree-collapsed`;
 const kTSTAPI_HAS_CHILD_TABS            = `${kTST_ID}:request:has-child-tabs`;
 const kTSTAPI_GET_DESCENDANT_TABS       = `${kTST_ID}:request:get-descendant-tabs`;
+const kTSTAPI_GET_TABS_STATE            = `${kTST_ID}:request:get-tabs-state`;
+const kTSTAPI_ADD_TABS_STATE            = `${kTST_ID}:notify:add-tabs-state`;
+const kTSTAPI_REMOVE_TABS_STATE         = `${kTST_ID}:notify:remove-tabs-state`;
 
 
 function onMessageExternal(aMessage, aSender) {
   console.log('onMessageExternal: ', aMessage, aSender);
   switch (aMessage.type) {
-    case kTSTAPI_NOTIFY_TAB_CLICKED: {
+    case kTSTAPI_NOTIFY_TAB_CLICKED: return (async () => {
       if (aMessage.button != 0)
-        return;
+        return false;
+
+      let tabIds = [aMessage.id];
+      if (aMessage.states.indexOf('subtree-collapsed') > -1) {
+        let descendantIds = await browser.runtime.sendMessage(kTST_ID, {
+          type: kTSTAPI_GET_DESCENDANT_TABS,
+          id:   aMessage.id
+        });
+        console.log('descendantIds ', descendantIds);
+        tabIds = tabIds.concat(descendantIds);
+      }
+      console.log('tabIds ', tabIds);
       if (aMessage.ctrlKey) {
         // toggle selection of the tab and all collapsed descendants
+        browser.runtime.sendMessage(kTST_ID, {
+          type:  aMessage.states.indexOf('selected') < 0 ?
+                   kTSTAPI_ADD_TABS_STATE :
+                   kTSTAPI_REMOVE_TABS_STATE,
+          ids:   tabIds,
+          value: 'selected'
+        });
+        return true;
       }
       else if (aMessage.shiftKey) {
         // select the clicked tab and tabs between last activated tab
       }
-    }; break;
+      return false;
+    })();
   }
 }
 
