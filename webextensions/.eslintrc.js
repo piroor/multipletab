83bca20ea06882cbd2a665fceb5c2a84c771268e
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-12 15:32:20 +0900

    Introduce ESLint for lint and formatting

---
+++ webextensions/.eslintrc.js
@@ -0,0 +1,34 @@
+/*eslint-env commonjs*/
+/*eslint quote-props: ['error', "always"] */
+
+module.exports = {
+  'root': true,
+
+  'parserOptions': {
+    'ecmaVersion': 2017,
+  },
+
+  'env': {
+    'webextensions': true,
+  },
+
+  'rules': {
+    // stylisitc problem
+    'indent': ['warn', 2, {
+      'SwitchCase': 1,
+      'MemberExpression': 1,
+      'CallExpression': {
+        'arguments': 'first',
+      },
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3,
+      }
+    }],
+    'quotes': ['warn', 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true,
+    }],
+  }
+};
