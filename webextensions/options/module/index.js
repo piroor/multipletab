7548d0eac0bb9f695824596c45e9b696cba80e4f
YUKI Hiroshi <shimoda@clear-code.com>
2018-07-03 19:14:25 +0900

    Load external modules as ES modules

--- webextensions/options/module/index.js
+++ webextensions/options/module/index.js
@@ -7,12 +7,24 @@
 import * as Constants from '../../common/module/constants.js';
 import * as Common from '../../common/module/common.js';
 import * as Commands from '../../common/module/commands.js';
+import MenuUI from '../../extlib/MenuUI.js';
+import Options from '../../extlib/Options.js';
+import RichConfirm from '../../extlib/RichConfirm.js';
+import ShortcutCustomizeUI from '../../extlib/ShortcutCustomizeUI.js';
+import TabFavIconHelper from '../../extlib/TabFavIconHelper.js';
 import TabIdFixer from '../../extlib/TabIdFixer.js';
+import l10n from '../../extlib/l10n.js';
 
 window.Constants = Constants;
 window.Common = Common;
 window.Commands = Commands;
+window.MenuUI = MenuUI;
+window.Options = Options;
+window.RichConfirm = RichConfirm;
+window.ShortcutCustomizeUI = ShortcutCustomizeUI;
+window.TabFavIconHelper = TabFavIconHelper;
 window.TabIdFixer = TabIdFixer;
+window.l10n = l10n;
 
 window.log = Common.log;
 window.notify = Common.notify;
