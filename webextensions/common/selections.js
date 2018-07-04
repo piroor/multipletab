487cbfb8556a9ff05e86740e40ce11337d11e6f9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2018-07-04 23:15:10 +0900

    iAdd missing module

---
+++ webextensions/common/selections.js
@@ -0,0 +1,41 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+'use strict';
+
+import Selection from './selection.js';
+
+import EventListenerManager from '../extlib/EventListenerManager.js';
+
+export const onCreated = new EventListenerManager();
+
+const mSelections = new Map();
+
+export function get(windowId) {
+  let selection = mSelections.get(windowId);
+  if (!selection) {
+    selection = new Selection(windowId);
+    mSelections.set(windowId, selection);
+    onCreated.dispatch(selection);
+  }
+  return selection;
+}
+
+export function getAll() {
+  return Array.from(mSelections.values());
+}
+
+export async function getActive() {
+  return get((await browser.windows.getLastFocused()).id);
+}
+
+browser.windows.onRemoved(windowId => {
+  if (!mSelections.has(windowId))
+    return;
+  const selection = mSelections.get(windowId);
+  selection.onChanged.removeAllListeners();
+  selection.clear();
+  mSelections.delete(windowId);
+});
