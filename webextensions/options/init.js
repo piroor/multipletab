aa853fe3b80e85f7bbb38de8511407ae143ccf08
YUKI Hiroshi <shimoda@clear-code.com>
2018-11-21 18:05:24 +0900

    Suppress error on format removal

--- webextensions/options/init.js
+++ webextensions/options/init.js
@@ -224,6 +224,7 @@ function createFormatRow(params = {}) {
 function onRowControlButtonClick(event) {
   const button = getButtonFromEvent(event);
   const row = button.parentNode.parentNode;
+  const container = row.parentNode;
   const formats = configs.copyToClipboardFormats;
   const item = formats[row.itemIndex];
   if (button.classList.contains('remove')) {
@@ -247,7 +248,7 @@ function onRowControlButtonClick(event) {
       row.parentNode.insertBefore(row, row.nextSibling.nextSibling);
     }
   }
-  Array.slice(row.parentNode.childNodes).forEach((aRow, index) => {
+  Array.slice(container.childNodes).forEach((aRow, index) => {
     aRow.itemIndex = index;
   });
 }
