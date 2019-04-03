17216a37809ab2aabc7397b6db61bedc0ec63063
YUKI Hiroshi <shimoda@clear-code.com>
2019-04-03 12:36:18 +0900

    Remove obsolete resources for the legacy version

--- make.bat
+++
@@ -1,3 +0,0 @@
-copy makexpi\makexpi.sh .\
-bash makexpi.sh -n multipletab -o
-del makexpi.sh
--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-appname=multipletab
-
-cp makexpi/makexpi.sh ./
-./makexpi.sh -n $appname -o
-rm ./makexpi.sh
-
